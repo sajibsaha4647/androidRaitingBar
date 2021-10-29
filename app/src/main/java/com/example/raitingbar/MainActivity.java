93df23011e95a728eb413bfff6269929bb12d1de
sajibsaha4647 <sksaha4648@gmail.com>
2021-10-29 14:21:31 +0600

    first commit

---
+++ app/src/main/java/com/example/raitingbar/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.raitingbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.RatingBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private RatingBar ratingBar;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ratingBar = findViewById(R.id.Raitingbar);
+        textView  = findViewById(R.id.showtext);
+        textView.setText("your raiting is" + ratingBar.getProgress());
+
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
+                        textView.setText("your raiting is" + rating);
+            }
+        });
+
+    }
+}
